777dea1b49b74729757807f292ad639fa3c59f8f
sty-sharif <stysharif@gmail.com>
2022-07-18 19:01:55 +0430

    add edit/update v2

--- src/components/todo/View.js
+++ src/components/todo/View.js
@@ -1,84 +1,82 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTrash,
-  faPencil,
-  faPencilSquare,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
 
 const View = () => {
   const [array, setArray] = useState(["Person1", "Person2", "Person3"]);
-  const [name, setName] = useState("");
+  const [nameToAdd, setNameToAdd] = useState("");
+  const [editingIndex, setEditingIndex] = useState(null);
 
-  const Add = () => {
-    setArray([...array, name]);
+  const inputRef = useRef();
+
+  const addName = () => {
+    setArray([...array, nameToAdd]);
+    setNameToAdd("");
+    inputRef.current.focus();
   };
 
-  const Delete = (name) => {
-    setArray(array.filter((e) => e !== name));
+  const deleteName = (index) => {
+    setArray(array.filter((idx) => idx !== index));
+    setEditingIndex(null);
   };
 
-  const Edit = (item, index) => {
-   // console.log("===> setName => ", item, index);
-    setName(item);
+  const editIndex = (index) => {
+    if (index === editingIndex) {
+      setEditingIndex(null);
+    } else {
+      setEditingIndex(index);
+    }
   };
 
-  const Update = (item, name, index) => {
-   // console.log("===> setName => ", item, index, name);
-    array[index] = name;
-    setName(item);
-    //console.log(array);
+  const updateName = (value, indexToUpdate) => {
+    setArray(array.map((item, idx) => (idx === indexToUpdate ? value : item)));
   };
+
   return (
-    <>
+    <div>
       <h1>TO DO LIST</h1>
       <hr />
-      <input onChange={(e) => setName(e.target.value)} type="text" />
-      <Button variant="primary" onClick={Add}>
+      <input
+        ref={inputRef}
+        value={nameToAdd}
+        onChange={(e) => setNameToAdd(e.target.value)}
+        type="text"
+      />
+      <Button variant="primary" onClick={addName}>
         Add Name
       </Button>
       <hr />
       {array.map((item, index) => (
-        <ul className="list-group">
-          <li key={index}>
-            {item}{" "}
+        <ul className="list-group" key={index}>
+          <li>
+            {index === editingIndex ? (
+              <input
+                value={item}
+                onChange={(e) => updateName(e.target.value, index)}
+              />
+            ) : (
+              item
+            )}{" "}
             <span className="badge">
               <span className="badge">
-                <Button variant="warning" onClick={(e) => Edit(item, index)}>
+                <Button
+                  variant={index === editingIndex ? "success" : "warning"}
+                  onClick={() => editIndex(index)}
+                >
                   <FontAwesomeIcon icon={faPencil} />
                 </Button>
               </span>
-              <Button variant="danger" onClick={(e) => Delete(item)}>
+
+              <Button variant="danger" onClick={() => deleteName(index)}>
                 <FontAwesomeIcon icon={faTrash} />
               </Button>
             </span>
           </li>
         </ul>
       ))}
-      <hr />
-      {array.map((item, index) => (
-        <ul className="list-group">
-          <li key={index}>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <span className="badge">
-              <Button
-                variant="success"
-                value={name}
-                onClick={(e) => Update(item, name, index)}
-              >
-                <FontAwesomeIcon icon={faPencilSquare} />
-              </Button>
-            </span>
-          </li>
-        </ul>
-      ))}
-    </>
+    </div>
   );
 };
 
